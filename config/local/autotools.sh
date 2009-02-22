54f0edb964b4f0153db532c7687db10e70ea80e2
Ondrej Certik <ondrej@certik.cz>
2009-02-22 12:59:57 -0800

    Remove parts not directly required to build the package

--- config/local/autotools.sh
+++
@@ -1,5 +0,0 @@
-# Autotools version information
-abi_m4_version="010411"
-abi_ac_version="026200"
-abi_am_version="011001"
-abi_lt_version="020204"
--- util/misc/change_perl.sh
+++
@@ -1,2 +0,0 @@
-# Copyright (C) 1998-2008 ABINIT group (XG)
- sed -e 's&#CODE_PAR&$CODE_PAR&' Utilities/,perl_tmp > Utilities/,perl_location_command
